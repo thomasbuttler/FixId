2c5d42732e1d2f42bbb1d494301b471fd76059e4
thomasbuttler <43081771+thomasbuttler@users.noreply.github.com>
2019-07-14 16:13:16 -0400

    Fix typo.

--- test/FixId_test.exs
+++ test/FixId_test.exs
@@ -2,7 +2,7 @@ defmodule FixIdTest do
   use ExUnit.Case
 
   test "run main" do
-    assert FixId.main(["test/ids_file", "test/dir"]) == ok
+    assert FixId.main(["test/ids_file", "test/dir"]) == :ok
   end
 
 end
